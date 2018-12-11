1aa2802e8c7d85fd3beb107e18ebc07b168e59ff
JoungKyun Kim <joungkyun@gmail.com>
2018-12-11 22:46:26 +0900

    update kldpfy

---
+++ kldp/QA/sync.sh
@@ -0,0 +1,5 @@
+curl -o 2002/12/qa-small.png https://www.w3.org/QA/2002/12/qa-small.png
+curl -o Tools/I_heart_validator.png https://www.w3.org/QA/Tools/I_heart_validator.png
+curl -o Tools/I_heart_validator_lg.png https://www.w3.org/QA/Tools/I_heart_validator_lg.png
+curl -o Tools/don_prog.js https://www.w3.org/QA/Tools/don_prog.js
+curl -o Tools/w3c-include.js https://www.w3.org/QA/Tools/w3c-include.js
